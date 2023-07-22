2725f08de3a25a906c839b2c0f412e0796362372
tfhmc <130200297+tfhmc@users.noreply.github.com>
2023-07-22 21:16:32 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,6 @@
-<?php    
-$str=file_get_contents('https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');    
-if(preg_match("/<url>(.+?)<\/url>/",$str,$matches)){       
-  $imgurl='https://cn.bing.com'.$matches[1];  
-}   
-if($imgurl){        
-  header('Content-Type: image/JPEG');      
-  @ob_end_clean();       
-  @readfile($imgurl);      
-  @flush(); @ob_flush();     
-  exit();   
-}else{    
-  exit('error');   
-}
+<?php
+    $str = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');   //读取必应api
+    $str = json_decode($str,true);                            //接受JSON 编码的字符串并且把它转换为 PHP变量
+    $imgurl = 'https://cn.bing.com'.$str['images'][0]['url'];                               //获取图片url
+    header("Location: {$imgurl}");                                                      // 跳转至目标图像
 ?>
